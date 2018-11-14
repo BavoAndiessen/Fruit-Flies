791844ba150747f2fa45e02c303114408b93e67b
DreVanhoucke <44637983+DreVanhoucke@users.noreply.github.com>
2018-11-14 12:04:58 +0100

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -2,7 +2,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class MyWorld extends World
 {  
-    public int i = 0;
+    public int FlyCount = 0;
     
     public MyWorld()
     {    
@@ -15,27 +15,28 @@ public class MyWorld extends World
     }
 
     public void act()
-        {      
-          while (i <= 20)
-            {
+        { 
+          int kans = Greenfoot.getRandomNumber(200);
+          
+          if (kans < 10 && FlyCount < 20)
+          {
+                int geslacht = Greenfoot.getRandomNumber(3);
 
-            	int kans = Greenfoot.getRandomNumber(10000);
-            	int geslacht = Greenfoot.getRandomNumber(3);
+                if (geslacht == 1)
+                {
+                    Fly fly = new Fly(true);
+                    addObject(fly,30,74);
+                    FlyCount++;
+                }
 
-            	if (kans < 20 && geslacht == 1)
-            	{
-            		Fly fly = new Fly(true);
-                	addObject(fly,30,74);            
-                	i++; 
-            	}
-
-            	if (kans < 20 && geslacht == 2)
-            	{
-            		Fly fly = new Fly(false);
-                	addObject(fly,30,74);
-                	i++; 
-            	}
-                
-            } 
+                if (geslacht == 2)
+                {
+                    Fly fly = new Fly(false);
+                    addObject(fly,30,74);
+                    FlyCount++;
+                }
+            
+            }
         }
+
 }
