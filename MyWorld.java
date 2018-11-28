bf607868115bc73632ee43ca5a72f8aca5f95f71
DreVanhoucke <44637983+DreVanhoucke@users.noreply.github.com>
2018-11-28 09:03:23 +0100

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -5,10 +5,12 @@ public class MyWorld extends World
     public int FlyCount = 0; //het is niet de bedoeling dat deze int verandert wanneer een vlieg dood gaat
     public int FruitCount = 0;
     public int FlyCountDisplay = 0;
+    public static int lvl = 1;
     public static int Score = 0;
     
     Label FlyDisplay = new Label("Aantal vliegen: 0");
     Label Scorebord = new Label("Score: 0");
+    Label Level = new Label("Level: 1");
     
     public MyWorld()
     {    
@@ -19,15 +21,57 @@ public class MyWorld extends World
         
         addObject (FlyDisplay,500, 600);
         addObject (Scorebord,500, 620);
+        addObject (Level, 500, 640);
+        
+        Vliegenmepper Vliegenmepper = new Vliegenmepper();
+        addObject (Vliegenmepper, 100, 100);
 
         act();
     }
 
     public void act()
-        { 
-          FlyCountDisplay = FlyCount - Score;
-            
-          int kansNewFly = Greenfoot.getRandomNumber(1000);
+    { 
+        SpawnVlieg();
+        labels();
+        spawnFruit();  
+    }
+        
+    public void spawnFruit()
+    {
+        int kansNewFruit = Greenfoot.getRandomNumber(10000);
+          
+          if (kansNewFruit < 10 && FruitCount < 2)
+          {
+              int fruitsoort = Greenfoot.getRandomNumber(6);
+              
+              if (fruitsoort == 1)
+              {
+                  
+              }
+              
+              //if statement voor elke fruitsoort net zoals bij de vliegen
+          }
+    }
+        
+    public void labels()
+    {
+        FlyDisplay.setText("Aantal vliegen: "+ FlyCountDisplay);
+        Scorebord.setText("Score: " + Score); //+ score vanuit Spray/mepper klasse
+        Level.setText("Level: " + lvl);
+        
+        FlyCountDisplay = FlyCount - Score;
+          
+          if (Score == 20)
+          {
+              Score = 0;
+              lvl++;
+              FlyCount = 0;
+          }
+    }
+        
+    public void SpawnVlieg()
+    {
+        int kansNewFly = Greenfoot.getRandomNumber(1000);
           
           if (kansNewFly < 10 && FlyCount < 20)
           {
@@ -48,25 +92,5 @@ public class MyWorld extends World
                 }
             
             }
-            
-          int kansNewFruit = Greenfoot.getRandomNumber(10000);
-          
-          if (kansNewFruit < 10 && FruitCount < 2)
-          {
-              int fruitsoort = Greenfoot.getRandomNumber(6);
-              
-              //if statement voor elke fruitsoort net zoals bij de vliegen
-          }
-          
-          //spawn Vliegenmepper en Spray in world
-            
-          FlyDisplay.setText("Aantal vliegen: "+ FlyCountDisplay);
-          Scorebord.setText("Score: " + Score); //+ score vanuit Spray/mepper klasse
-          
-          if (Score == 20)
-          {
-              Greenfoot.stop();
-          }
-        }
-
+    }
 }
