b83404d530303f1a2f4ace895b10d46d54801f9a
DreVanhoucke <44637983+DreVanhoucke@users.noreply.github.com>
2018-11-28 08:49:45 +0100

    Add files via upload

--- MyWorld.java
+++ MyWorld.java
@@ -1,21 +1,96 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class MyWorld extends World
-{
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
+{  
+    public int FlyCount = 0; //het is niet de bedoeling dat deze int verandert wanneer een vlieg dood gaat
+    public int FruitCount = 0;
+    public int FlyCountDisplay = 0;
+    public static int lvl = 1;
+    public static int Score = 0;
+    
+    Label FlyDisplay = new Label("Aantal vliegen: 0");
+    Label Scorebord = new Label("Score: 0");
+    Label Level = new Label("Level: 1");
+    
     public MyWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(1159, 674, 1); 
+
+        GreenfootImage grondplan = new GreenfootImage("grondplan.PNG");
+        setBackground(grondplan); 
+        
+        addObject (FlyDisplay,500, 600);
+        addObject (Scorebord,500, 620);
+        addObject (Level, 500, 640);
+        
+        Vliegenmepper Vliegenmepper = new Vliegenmepper();
+        addObject (Vliegenmepper, 100, 100);
+
+        act();
+    }
+
+    public void act()
+    { 
+        SpawnVlieg();
+        labels();
+        spawnFruit();  
+    }
+        
+    public void spawnFruit()
+    {
+        int kansNewFruit = Greenfoot.getRandomNumber(10000);
+          
+          if (kansNewFruit < 10 && FruitCount < 2)
+          {
+              int fruitsoort = Greenfoot.getRandomNumber(6);
+              
+              if (fruitsoort == 1)
+              {
+                  
+              }
+              
+              //if statement voor elke fruitsoort net zoals bij de vliegen
+          }
+    }
+        
+    public void labels()
+    {
+        FlyDisplay.setText("Aantal vliegen: "+ FlyCountDisplay);
+        Scorebord.setText("Score: " + Score); //+ score vanuit Spray/mepper klasse
+        Level.setText("Level: " + lvl);
+        
+        FlyCountDisplay = FlyCount - Score;
+          
+          if (Score == 20)
+          {
+              Score = 0;
+              lvl++;
+              FlyCount = 0;
+          }
+    }
+        
+    public void SpawnVlieg()
+    {
+        int kansNewFly = Greenfoot.getRandomNumber(1000);
+          
+          if (kansNewFly < 10 && FlyCount < 20)
+          {
+                int geslacht = Greenfoot.getRandomNumber(3);
+
+                if (geslacht == 1)
+                {
+                    Fly fly = new Fly(true);
+                    addObject(fly,30,74);
+                    FlyCount++;
+                }
+
+                if (geslacht == 2)
+                {
+                    Fly fly = new Fly(false);
+                    addObject(fly,30,74);
+                    FlyCount++;
+                }
+            
+            }
     }
 }
