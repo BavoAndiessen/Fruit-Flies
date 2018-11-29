39ea5177bf8547f8c83cabe903351470818a87d3
robbeverduyn <44638305+robbeverduyn@users.noreply.github.com>
2018-11-29 19:45:06 +0100

    Update Aardbei.java

--- Aardbei.java
+++ Aardbei.java
@@ -1,35 +1,56 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Aardbei here.
+ * Write a description of class Appel here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
 public class Aardbei extends Fruit
 {
+    /**
+     * Act - do whatever the Appel wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
     private GreenfootImage aardbei;
     private GreenfootImage aardbeif1;
     private GreenfootImage aardbeif2;
-    public int levensduur = 300;
+    public int Timer = 0;
     public Aardbei() 
     {
         aardbei = new GreenfootImage("aardbei_transparant.png");
+        aardbei.scale(112,90);
         aardbeif1 = new GreenfootImage("aardbei_transparant 1.png");
+        aardbeif1.scale(112,90);
         aardbeif2 = new GreenfootImage("aardbei_transparant 2.png");
+        aardbeif2.scale(112,90);
+        setImage (aardbei);
         // Add your action code here.
-    }  
+    }   
+
     public void act()
     {
-        if ( getOneIntersectingObject(Fly.class) != null )
+        Fly fly = (Fly) getOneIntersectingObject(Fly.class);
+        if (fly != null)
         {
-            
-            Fly fly = (Fly) getOneIntersectingObject(Fly.class);
             fly.stop();
-        if (timer(levensduur) == 50)
-            fly.start();
+            Timer ++;
+            if (Timer == 1)
+            { 
+                setImage (aardbei);
+            }
+            if (Timer == 400)
+            {
+                setImage (aardbeif1);
+            }
+            if (Timer == 600)
+            {
+                setImage (aardbeif2);
+            }
+            if (Timer == 650)
+            {
+                getWorld().removeObject(this);
+            }
         }
-        getWorld().removeObject(this);
-    }
-    
+    }   
 }
