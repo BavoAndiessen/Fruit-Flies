83f27ffe527bb8b346225030218a135798af139c
DreVanhoucke <44637983+DreVanhoucke@users.noreply.github.com>
2018-12-03 17:58:22 +0100

    Add files via upload

---
+++ Scorebord.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Scorebord here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Scorebord extends Actor
+{
+    /**
+     * Act - do whatever the Scorebord wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        GreenfootImage scorebord = new GreenfootImage("scorebord.JPG");
+        setImage(scorebord);
+    }    
+}
